6d6ffc7f988ee2a43741e25a9da92e87f3495587
jiyuujin <jiyuujinunite@gmail.com>
2023-11-28 10:40:38 +0900

    NavigationTab on bottom - nekohack/base-widgets@c9b78cc

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:base_widgets/models/tab_item.dart';
+import 'package:flutter/material.dart';
+
+const title = 'Mileage Run';
+
+final List<TabItem> tabItems = [
+  TabItem(icon: Icons.home.codePoint, name: 'Home'),
+  TabItem(icon: Icons.favorite.codePoint, name: 'Favorite'),
+  TabItem(icon: Icons.settings.codePoint, name: 'Settings'),
+];
