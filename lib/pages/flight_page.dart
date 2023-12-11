603858fb01c72937816501a16998ee3c540a5880
jiyuujin <jiyuujinunite@gmail.com>
2023-12-11 14:22:21 +0900

    remove toString()

--- lib/pages/flight_page.dart
+++ lib/pages/flight_page.dart
@@ -43,11 +43,11 @@ class FlightList extends StatelessWidget {
             return Padding(
               padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
               child: BoardingCard(
-                boardedAt: DateFormat('yyyy-MM-ddTHH:mm:ss.000+09:00').parse(data['time'].toString()),
-                departure: getAirportName(int.parse(data['departure'].toString())),
-                arrival: getAirportName(int.parse(data['arrival'].toString())),
-                airline: getAirlineName(int.parse(data['airline'].toString())),
-                boardingType: getBoardingTypeName(int.parse(data['boardingType'].toString())),
+                boardedAt: DateFormat('yyyy-MM-ddTHH:mm:ss.000+09:00').parse(data['time']),
+                departure: getAirportName(int.parse(data['departure'])),
+                arrival: getAirportName(int.parse(data['arrival'])),
+                airline: getAirlineName(int.parse(data['airline'])),
+                boardingType: getBoardingTypeName(int.parse(data['boardingType'])),
                 registration: data['registration'],
               ),
             );
