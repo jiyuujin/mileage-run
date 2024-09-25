bffdc7833cc777423f351a369304fa9e61b35b5a
jiyuujin <jiyuujinunite@gmail.com>
2024-09-25 22:41:22 +0900

    signInGoogle

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:base_widgets/components/custom_button.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:mileage_run/auth/signin_google.dart';
 import 'package:mileage_run/constants.dart';
+import 'package:mileage_run/hooks/use_signin.dart';
+import 'package:mileage_run/pages/top_page.dart';
 
 import 'firebase_options.dart';
 
@@ -18,7 +21,7 @@ void main() async {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookWidget {
   const MyApp({super.key});
 
   @override
@@ -29,10 +32,33 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home: const TopPage(),
-      routes: <String, WidgetBuilder>{
-        '/': (_) => const SigninGoogle(),
-      },
+      home: const TopPage(),
+      // home: const SignInApp(),
+    );
+  }
+}
+
+class SignInApp extends HookWidget {
+  const SignInApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: Center(
+        child: SizedBox(
+          width: 200,
+          height: 30,
+          child: CustomButton(
+            backgroundColor: Colors.blue,
+            textColor: Colors.white,
+            borderRadius: BorderRadius.circular(8),
+            title: 'Google',
+            onPressed: () async {
+              await signInGoogle(context);
+            },
+          ),
+        ),
+      ),
     );
   }
 }
