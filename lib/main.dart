6fbb64f6e55f96bcb3858fecb5d1fc0d0da75ff9
jiyuujin <jiyuujinunite@gmail.com>
2024-11-03 15:16:22 +0900

    fix: auth provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:base_widgets/components/custom_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -7,6 +8,7 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:mileage_run/constants.dart';
 import 'package:mileage_run/hooks/use_signin.dart';
 import 'package:mileage_run/pages/top_page.dart';
+import 'package:mileage_run/providers/auth_provider.dart';
 
 import 'firebase_options.dart';
 
@@ -21,28 +23,29 @@ void main() async {
   ));
 }
 
-class MyApp extends HookWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    String? userEmail = ref.watch(userEmailProvider);
+
     return MaterialApp(
       title: title,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const TopPage(),
-      // home: const SignInApp(),
+      home: userEmail != null ? const TopPage() : const SignInApp(),
     );
   }
 }
 
-class SignInApp extends HookWidget {
+class SignInApp extends ConsumerWidget {
   const SignInApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
        body: Center(
         child: SizedBox(
@@ -54,7 +57,8 @@ class SignInApp extends HookWidget {
             borderRadius: BorderRadius.circular(8),
             title: 'Google',
             onPressed: () async {
-              await signInGoogle(context);
+              await signInGoogle(context)
+                .then((value) => ref.watch(userEmailProvider.notifier).state = value!.user!.email);
             },
           ),
         ),
