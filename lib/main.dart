df71f090bc26aecb3700edcb542d7d5c7d6ff1db
jiyuujin <jiyuujinunite@gmail.com>
2024-11-03 12:42:53 +0900

    fix: auth provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:base_widgets/components/custom_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -7,6 +8,7 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:mileage_run/constants.dart';
 import 'package:mileage_run/hooks/use_signin.dart';
 import 'package:mileage_run/pages/top_page.dart';
+import 'package:mileage_run/providers/auth_provider.dart';
 
 import 'firebase_options.dart';
 
@@ -21,18 +23,28 @@ void main() async {
   ));
 }
 
-class MyApp extends HookWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        ref.watch(userEmailProvider.notifier).state = null;
+      } else {
+        ref.watch(userEmailProvider.notifier).state = user.email!;
+      }
+    });
+
+    String? userEmail = ref.watch(userEmailProvider.notifier).state;
+
     return MaterialApp(
       title: title,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const TopPage(),
+      home: userEmail == null ? const TopPage() : const SignInApp(),
       // home: const SignInApp(),
     );
   }
