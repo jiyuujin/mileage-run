cf94d68c9c88353f68927ae804b5f8e1eb8c41c5
jiyuujin <y-kitamura@wevnal.co.jp>
2025-12-24 19:21:08 +0900

    fix: update widgets

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,3 +8,18 @@ final List<TabItem> tabItems = [
   TabItem(icon: Icons.photo_camera.codePoint, name: 'Photo'),
   TabItem(icon: Icons.auto_graph.codePoint, name: 'Graph'),
 ];
+
+final List<Map<String, String>> photoLinks = [
+  {
+    'url': 'https://photos.app.goo.gl/XxbgDmBbjxk1HqqU9',
+    'title': 'Frequent Flyer 2025',
+  },
+  {
+    'url': 'https://photos.app.goo.gl/VJmU2UvGcb3zxxwm9',
+    'title': 'Frequent Flyer 2024',
+  },
+  {
+    'url': 'https://photos.app.goo.gl/ZngZBdQ18QN7guKs7',
+    'title': 'Frequent Flyer 2023',
+  },
+];
--- lib/pages/photo_page.dart
+++ lib/pages/photo_page.dart
@@ -1,24 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:mileage_run/constants.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PhotoPage extends StatelessWidget {
   const PhotoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(color: Colors.lightBlue),
-      child: const FlightList(),
-    );
-  }
-}
-
-class FlightList extends StatelessWidget {
-  const FlightList({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const SizedBox(
-      child: Text('Photo Page'),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Photo Album')),
+      body: ListView.builder(
+        itemCount: photoLinks.length,
+        itemBuilder: (context, index) {
+          final link = photoLinks[index];
+          return ListTile(
+            title: Text(link['title']!),
+            trailing: IconButton(
+              icon: const Icon(Icons.open_in_new, color: Colors.green),
+              onPressed: () async {
+                final uri = Uri.parse(link['url']!);
+                if (await canLaunchUrl(uri)) {
+                  await launchUrl(uri, mode: LaunchMode.externalApplication);
+                }
+              },
+            ),
+            onTap: () async {
+              final uri = Uri.parse(link['url']!);
+              if (await canLaunchUrl(uri)) {
+                await launchUrl(uri, mode: LaunchMode.externalApplication);
+              }
+            },
+          );
+        },
+      ),
     );
   }
 }
